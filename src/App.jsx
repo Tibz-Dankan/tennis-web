151bc8ff5f728b7e866b06488d1024a001cdddae
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-12-01 10:19:22 +0300

    configure tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { PhaserGame } from "./game/PhaserGame";
 import { EventBridge } from "./game/bridge/EventBridge";
 import { GameControls } from "./game/controls/GameControls";
+import "./app.css";
 
 const App = () => {
   return (
-    <div id="app">
+    <div id="app" className="bg-primary flex flex-col items-center">
       <EventBridge />
       <PhaserGame />
       <GameControls />
