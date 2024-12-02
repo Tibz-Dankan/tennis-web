40f723e72f09476932bad146e0a7944e789a3347
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-12-02 03:32:46 +0300

    feat: add wallet bitcoin logo animation

--- src/game/statistics/Score.jsx
+++ src/game/statistics/Score.jsx
@@ -5,10 +5,10 @@ export const Score = () => {
   const humanScore = useScoreStore((state) => state.humanScore);
   const computerScore = useScoreStore((state) => state.computerScore);
   return (
-    <div className="w-full border-4 rounded-xl p-3 space-y-2s">
-      {/* <p className="flex items-center justify-center gap-2">
-        <span className="font-bold text-center">Score</span>
-      </p> */}
+    <div
+      className="w-full border-4 border-gray-300 rounded-xl 
+     p-3 space-y-2s text-gray-300"
+    >
       <p className="flex items-center justify-between gap-2">
         <span className="font-bold">You:</span>
         <span className="font-bold">{humanScore}</span>
